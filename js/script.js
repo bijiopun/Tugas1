42c85c00972bfcb4acc80f15f0bf1f7e64ba830e
bijiopun <jinggaguys@gmail.com>
2024-09-03 20:21:37 +0700

    add fitur login

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) { 
+    event.preventDefault();
+
+    const correctUsername = '1';
+    const correctPassword = '1';
+    
+    const username = document.getElementById('username').value; 
+    const password = document.getElementById('password').value;
+    
+    if(username === correctUsername && password === correctPassword){
+        alert('Login Berhasil');
+        window.location.href = 'index.html';
+    
+    }else{
+    
+    alert('login gagal');
+    
+    }
+});
